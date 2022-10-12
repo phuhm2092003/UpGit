f958280b38e1b6a33f595f724feec3fb001e8bc8
Hồ Minh Phú <108424905+phuhm2092003@users.noreply.github.com>
2022-10-12 23:04:52 +0700

    Update MainActivity.java

--- app/src/main/java/fpt/edu/demogit/MainActivity.java
+++ app/src/main/java/fpt/edu/demogit/MainActivity.java
@@ -13,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Task1", Toast.LENGTH_SHORT).show();
+
     }
 }
