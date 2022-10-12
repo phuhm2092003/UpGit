cc5b740e77a6f2e250410feb56167da0f4417007
DELL <hominphuzxc@gmail.com>
2022-10-12 23:03:47 +0700

    delete all file

--- app/src/main/java/fpt/edu/demogit/MainActivity.java
+++ app/src/main/java/fpt/edu/demogit/MainActivity.java
@@ -9,41 +9,9 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn, btn6, btn7, btn8;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Android studio
-        Toast.makeText(this, "Hello android studio", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Hello android studio1", Toast.LENGTH_SHORT).show();
-        // phú sửa
-        /// tú
-        Toast.makeText(this, "Helo word", Toast.LENGTH_SHORT).show();
-
-        // hello Hồ minh phú
-        btn = findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Hello my app", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "Android nâng cao", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        Toast.makeText(this, "CLick me", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Hello word", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "main", Toast.LENGTH_SHORT).show();
-        // mới đảy code
-        Toast.makeText(this, "mới đẩy", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "mới đẩy2", Toast.LENGTH_SHORT).show();
-
-        Toast.makeText(this, "mới đẩy4", Toast.LENGTH_SHORT).show();
-
-
-        Toast.makeText(this, "mới đẩy3", Toast.LENGTH_SHORT).show();
-
-
     }
 }
