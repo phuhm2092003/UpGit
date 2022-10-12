18c8c625d8125beb45d2212b4c71292a63fc93e4
DELL <hominphuzxc@gmail.com>
2022-10-12 14:25:30 +0700

    new task

--- app/src/main/java/fpt/edu/demogit/MainActivity.java
+++ app/src/main/java/fpt/edu/demogit/MainActivity.java
@@ -16,11 +16,6 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Hello android studio", Toast.LENGTH_SHORT).show();
         // b comment
         Toast.makeText(this, "Hồ mình phú", Toast.LENGTH_SHORT).show();
-        
-  
-        
-        
-
         Log.i("TAG", "onCreate: work của phú");
 
     }
