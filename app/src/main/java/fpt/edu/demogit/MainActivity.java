ac215b8b935f81abcaa9013e8a1e0a71dde270a9
Hồ Minh Phú <108424905+phuhm2092003@users.noreply.github.com>
2022-10-12 15:26:27 +0700

    Update MainActivity.java

--- app/src/main/java/fpt/edu/demogit/MainActivity.java
+++ app/src/main/java/fpt/edu/demogit/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn, btn6, btn7;
+    Button btn, btn6, btn7, btn8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
