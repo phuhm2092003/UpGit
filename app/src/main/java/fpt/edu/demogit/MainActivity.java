f68f796145550ea5893121aab9d18c2a428b1856
DELL <hominphuzxc@gmail.com>
2022-10-12 14:44:06 +0700

    click btn

--- app/src/main/java/fpt/edu/demogit/MainActivity.java
+++ app/src/main/java/fpt/edu/demogit/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
         // phú sửa
         /// tú
         Toast.makeText(this, "Helo word", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Helo word", Toast.LENGTH_SHORT).show();
         
        
 
