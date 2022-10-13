3930d9ccd80817eef413cc57904c99e37326a02c
DELL <hominphuzxc@gmail.com>
2022-10-13 10:13:30 +0700

    add

--- app/src/main/java/fpt/edu/demogit/MainActivity.java
+++ app/src/main/java/fpt/edu/demogit/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Hello Android", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Hello Android2", Toast.LENGTH_SHORT).show();
             }
         });
     }
