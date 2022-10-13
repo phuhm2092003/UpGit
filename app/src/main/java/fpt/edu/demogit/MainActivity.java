0e7cac9270175a87574cae0a8f54165ee01541f8
DELL <hominphuzxc@gmail.com>
2022-10-13 09:26:58 +0700

    Udate master

--- app/src/main/java/fpt/edu/demogit/MainActivity.java
+++ app/src/main/java/fpt/edu/demogit/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Toast.makeText(MainActivity.this, "Hello wold", Toast.LENGTH_SHORT).show();
             }
         });
     }
